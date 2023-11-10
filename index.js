a82533d5083ea69dcc45dc52a669d438aa2d76c4
Saicharan-Yadav <saicharanyadav.p20@iiits.in>
2023-11-10 23:53:57 +0530

    tc

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
 
 const middleware = jsonServer.defaults();
 const ports = process.env.PORT || 8000;
 
 server.unsubscribe(middleware);
 server.use(router);
+server.use(cors());
 
 server.listen(ports, () => {
   console.log("Server is running");
